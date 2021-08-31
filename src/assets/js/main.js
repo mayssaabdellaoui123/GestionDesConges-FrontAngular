1c384ec6d8db4d004e47bfc59dc3e7981397b7a6
mayssaabdellaoui123 <58725994+mayssaabdellaoui123@users.noreply.github.com>
2021-08-31 13:41:38 +0200

    yyy

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,33 +1,68 @@
+
 (function ($) {
-	
-	/*============== JS FILE ACTIVE ===============*/
-	/*-----------------------------------------
-	[1. jQuery Mobilemenu ]
-	[2. Bootstrap Tabs ]
-	[3. ScrollUp ]
-	[4. Google Map ]
-	--------------------------------------------*/
-	/*---------------------
-	 [1. jQuery Mobilemenu ]
-	--------------------- */
-		jQuery('nav#dropdown').meanmenu();
-		
-	/*---------------------
-	[2. Bootstrap Tabs ]
-	--------------------- */
-	$('#myTabs a').on('click',function (e) {
-		e.preventDefault()
-		$(this).tab('show')
-	});
-	/*-------------------
-	[3. ScrollUp ]
-	---------------------*/
-	$.scrollUp({
-		animation: 'slide', // Fade, slide, none
-		scrollSpeed: 1500,
-		scrollText: [
-		  "<i class='fa fa-chevron-up'></i>"
-		]
-	});
-	
+    "use strict";
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+
 })(jQuery);
