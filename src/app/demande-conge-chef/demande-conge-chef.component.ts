c2fc76f1830df6af534de9c6f54dbe1884d1fc8f
alaa-fradi <alaafradi@gmail.com>
2021-09-26 00:09:48 +0100

    Revert "fi ziyeda ifeda"
    This reverts commit df916a2e3f755bd0cb734536549f4254e6abd41f.

--- src/app/demande-conge-chef/demande-conge-chef.component.ts
+++ src/app/demande-conge-chef/demande-conge-chef.component.ts
@@ -67,8 +67,8 @@ export class DemandeCongeChefComponent implements OnInit {
 
 
   vueDetails(){
-    let selectedconge = this.Conge[this.add1]
-    let data = selectedconge.idConge;
+    let selectedproduct = this.Conge[this.add1]
+    let data = selectedproduct.idConge;
    // console.log(idConge);
      console.log(data);
      this.serviceConge.getDetailsUserByIdConge(data).subscribe(
